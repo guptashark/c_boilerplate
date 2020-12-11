ca9f88764d2771456d90fa79b189f5580bc9a10d
Aishwary Gupta <guptashark@protonmail.com>
2020-12-10 20:54:46 -0500

    Improve the vector_push_back testing.

--- containers/vector/tests/test_push_back.c
+++ containers/vector/tests/test_push_back.c
@@ -18,8 +18,32 @@ int test_push_back( void ) {
 	vector_ctor ( & v );
 
 	vector_push_back ( & v, NULL );
-
 	assert ( v.size == 1 );
+
+	int x = 0;
+	int * x_ptr = & x;
+
+	vector_push_back ( & v, x_ptr );
+	assert ( v.size == 2 );
+
+	uint32_t running_size = 2;
+
+	// sort of an odd assertion,
+	// but what kind of init fn would
+	// allocate more than 1000. Plus, the
+	// point of this test is to make sure
+	// that we are in fact causing a realloc
+	// at some point so that we can test
+	// whether the vector handles it
+	// correctly.
+	assert ( v.capacity < 1000 );
+
+	for ( int i = 0; i < 1000; i++ ) {
+		vector_push_back ( & v, x_ptr );
+		running_size++;
+		assert ( v.size == running_size );
+		assert ( v.capacity >= v.size );
+	}
 
 	vector_dtor ( & v );
 
