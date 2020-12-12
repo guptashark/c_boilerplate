da38ce2bf6fa1fbf2f00bf328b0aeb417df27ab8
Aishwary Gupta <guptashark@protonmail.com>
2020-12-12 16:44:14 -0500

    Add more list tests.

--- containers/list/tests/test_list_comprehensive.c
+++ containers/list/tests/test_list_comprehensive.c
@@ -3,13 +3,46 @@
 
 #include "list.h"
 
+int test_list_push_front();
 int test_list_clear();
 int test_list_comprehensive ( void );
 
 int main ( void ) {
 
+	test_list_push_front();
 	test_list_clear();
 	test_list_comprehensive();
+	return 0;
+}
+
+int test_list_push_front ( void ) {
+
+	struct list s_obj;
+	struct list * s = & s_obj;
+
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+	list_ctor ( s );
+
+	list_push_front ( s, & x );
+	assert ( list_front ( s ) == & x );
+	assert ( list_size ( s ) == 1 );
+
+	list_push_front ( s, & y );
+	assert ( list_front ( s ) == & y );
+	assert ( list_size ( s ) == 2 );
+
+	list_pop_front ( s );
+	assert ( list_front ( s ) == & x );
+	assert ( list_size ( s ) == 1 );
+
+	list_push_front ( s, & z );
+	assert ( list_front ( s ) == & z );
+	assert ( list_size ( s ) == 2 );
+
+	list_dtor ( s );
 	return 0;
 }
 
