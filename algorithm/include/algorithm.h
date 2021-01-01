39f428a8e01b2f1a7137376a18d85719d3c5a236
Aishwary Gupta <guptashark@protonmail.com>
2021-01-01 11:08:57 -0500

    Implement the algo_any_of fn.

--- algorithm/include/algorithm.h
+++ algorithm/include/algorithm.h
@@ -9,6 +9,20 @@ algo_all_of(
 	struct vector_iter * it_last,
 	bool (* unary_predicate)(void *) );
 
+// Purpose:
+// Determines if any of the values in between the two iterators
+// satisfies the predicate.
+//
+// Return value:
+// Returns true if the predicate returns true for any value between the
+// iterators. Returns false otherwise. In the case of no elements,
+// the fn returns false.
+bool
+algo_any_of(
+	struct vector_iter * it_first,
+	struct vector_iter * it_last,
+	bool (* unary_predicate)(void *) );
+
 // for these fns to work, we need to pass a vtable that has all the
 // different functions to use on the given iterators.
 //
--- algorithm/src/algorithm.c
+++ algorithm/src/algorithm.c
@@ -18,6 +18,24 @@ algo_all_of(
 	return true;
 }
 
+bool
+algo_any_of(
+	struct vector_iter * it_first,
+	struct vector_iter * it_last,
+	bool (* unary_predicate)(void *)
+) {
+	while ( vector_iter_neq ( it_first, it_last) ) {
+		void * val = vector_iter_deref ( it_first );
+		if ( unary_predicate ( val ) == true ) {
+			return true;
+		}
+
+		vector_iter_inc ( it_first );
+	}
+
+	return false;
+}
+
 void
 algo_for_each(
 	struct vector_iter * it_first,
